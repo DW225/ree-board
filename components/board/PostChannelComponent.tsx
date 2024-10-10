50425876d24f8f3f2ba7cb052ea06c118ae36872
Andrew Lee <andrewjkes@hotmail.com>
2024-10-10 22:15:27 +0800

    feat: Add logic for receive vote update

--- components/board/PostChannelComponent.tsx
+++ components/board/PostChannelComponent.tsx
@@ -5,6 +5,8 @@ import {
   addPost,
   updatePostContent,
   removePost,
+  incrementPostVoteCount,
+  decrementPostVoteCount,
 } from "@/lib/signal/postSignals";
 import { EVENT_TYPE } from "@/lib/utils/ably";
 import { useChannel } from "ably/react";
@@ -22,12 +24,22 @@ export default function PostChannel({
     if (message.extras.headers.user !== userId) {
       const messageType = message.name;
       const post: Post = JSON.parse(message.data);
-      if (messageType === EVENT_TYPE.POST.ADD) {
-        addPost(post);
-      } else if (messageType === EVENT_TYPE.POST.UPDATE_CONTENT) {
-        updatePostContent(post.id, post.content);
-      } else if (messageType === EVENT_TYPE.POST.DELETE) {
-        removePost(post.id);
+      switch (messageType) {
+        case EVENT_TYPE.POST.ADD:
+          addPost(post);
+          break;
+        case EVENT_TYPE.POST.UPDATE_CONTENT:
+          updatePostContent(post.id, post.content);
+          break;
+        case EVENT_TYPE.POST.DELETE:
+          removePost(post.id);
+          break;
+        case EVENT_TYPE.POST.UPVOTE:
+          incrementPostVoteCount(post.id);
+          break;
+        case EVENT_TYPE.POST.DOWNVOTE:
+          decrementPostVoteCount(post.id);
+          break;
       }
     }
   });
--- lib/signal/postSignals.ts
+++ lib/signal/postSignals.ts
@@ -2,9 +2,10 @@ import type { Post, PostType } from "@/db/schema";
 import type { Signal } from "@preact/signals-react";
 import { signal } from "@preact/signals-react";
 
-export interface PostSignal extends Omit<Post, "content" | "type"> {
+export interface PostSignal extends Omit<Post, "content" | "type" | "voteCount"> {
   content: Signal<string>;
   type: Signal<PostType>;
+  voteCount: Signal<number>;
 }
 
 export const postSignal = signal<PostSignal[]>([]);
@@ -14,6 +15,7 @@ export const postSignalInitial = (posts: Post[]) => {
     ...post,
     content: signal(post.content),
     type: signal(post.type),
+    voteCount: signal(post.voteCount),
   }));
 };
 
@@ -24,6 +26,7 @@ export const addPost = (newPost: Post) => {
       ...newPost,
       content: signal(newPost.content),
       type: signal(newPost.type),
+      voteCount: signal(newPost.voteCount),
     },
   ];
 };
@@ -49,5 +52,19 @@ export const updatePostType = (postID: string, newType: PostType) => {
   const index = postSignal.value.findIndex((post) => post.id === postID);
   if (index!== -1) {
     postSignal.value[index].type.value = newType;
+  }
+};
+
+export const incrementPostVoteCount = (postID: string) => {
+  const index = postSignal.value.findIndex((post) => post.id === postID);
+  if (index!== -1) {
+    postSignal.value[index].voteCount.value++;
+  }
+};
+
+export const decrementPostVoteCount = (postID: string) => {
+  const index = postSignal.value.findIndex((post) => post.id === postID);
+  if (index!== -1 && postSignal.value[index].voteCount.value > 0) {
+    postSignal.value[index].voteCount.value--;
   }
 };
