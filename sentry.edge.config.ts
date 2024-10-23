fb91640fd2e0f427b4572ed741ac15884c66e778
Andrew Lee <andrewjkes@hotmail.com>
2024-10-23 14:34:11 +0800

    feat: Reduce sentry sample rate
    This is set to reduce the used of vercel function invocation

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
   dsn: "https://6da29c0bb73e98e12801cc0d58a8d8d0@o4508019925712896.ingest.us.sentry.io/4508019929776128",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
