4aaaee27932c23fa73db98c1767d92f88b76e3cd
Andrew Lee <andrewjkes@hotmail.com>
2024-11-02 16:51:22 +0800

    feat: Make avatarStack react to member update through signal

--- components/board/AvatarStack.tsx
+++ components/board/AvatarStack.tsx
@@ -1,27 +1,19 @@
+"use client";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import type { Role } from "@/db/schema";
+import { memberSignal } from "@/lib/signal/memberSingals";
+import { computed } from "@preact/signals-react";
+import { useSignals } from "@preact/signals-react/runtime";
 import MD5 from "crypto-js/md5";
 
-interface AvatarStackProps {
-  members: {
-    id: string;
-    userId: string;
-    role: Role;
-    username: string;
-    email: string;
-    updateAt: Date;
-  }[];
-}
-
-export default async function AvatarStack({
-  members,
-}: Readonly<AvatarStackProps>) {
-  const memberCount = members.length;
+export default function AvatarStack() {
+  useSignals();
+  const memberCount = computed(() => memberSignal.value.length);
 
   return (
     <div className="flex -space-x-3 hover:-space-x-1 transition-all duration-200 cursor-pointer">
-      {members.slice(0, 5).map((member, index) => (
-        <Avatar key={index} className="border-2 border-background">
+      {memberSignal.value.slice(0, 5).map((member) => (
+        <Avatar key={member.id} className="border-2 border-background">
           <AvatarImage
             src={`https://www.gravatar.com/avatar/${MD5(
               member.email
@@ -33,9 +25,9 @@ export default async function AvatarStack({
           </AvatarFallback>
         </Avatar>
       ))}
-      {memberCount > 5 && (
+      {memberCount.value > 5 && (
         <Avatar className="border-2 border-background">
-          <AvatarFallback>+{members.length - 5}</AvatarFallback>
+          <AvatarFallback>+{memberCount.value - 5}</AvatarFallback>
         </Avatar>
       )}
     </div>
