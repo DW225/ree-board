b8c861f0af167e5286c9897e530bf5855c46f779
Andrew Lee <andrewjkes@hotmail.com>
2025-03-03 09:37:24 +0800

    feat(Sentry): update Sentry configuration and increase trace sample rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -7,17 +7,8 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://6da29c0bb73e98e12801cc0d58a8d8d0@o4508019925712896.ingest.us.sentry.io/4508019929776128",
 
-  // Add optional integrations for additional features
-  integrations: [
-  ],
-
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 0.1,
-
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  tracesSampleRate: 0.2,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
